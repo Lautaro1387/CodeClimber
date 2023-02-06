e735c4743cba1068d11ca833f1ddfd1179428e9a
MarceloRB1 <105667144+MarceloRB1@users.noreply.github.com>
2023-02-06 17:11:07 -0300

    upodate changes for login and register page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,21 @@
-import './index.css';
-const menuButton = document.querySelector("#menu-button");
-const menu = document.querySelector("#menu");
-
-menuButton.addEventListener("click", () => {
-  menu.style.display = menu.style.display === "block" ? "none" : "block";
-});
+import React, { useState } from "react";
+import logo from './logo.svg';
+import './App.css';
+import { Login } from "./Login";
+import { Register } from "./Register";
 
 function App() {
+  const [currentForm, setCurrentForm] = useState('login');
+
+  const toggleForm = (formName) => {
+    setCurrentForm(formName);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      {
+        currentForm === "login" ? <Login onFormSwitch={toggleForm} /> : <Register onFormSwitch={toggleForm} />
+      }
     </div>
   );
 }
