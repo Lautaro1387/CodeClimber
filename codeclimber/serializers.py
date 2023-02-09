4211faef640bdc0713685b4aa87aefc4c5351410
vicomarquez <vicomarquezmuniz@gmail.com>
2023-02-09 15:35:14 -0300

    fix issues

--- codeclimber/serializers.py
+++ codeclimber/serializers.py
@@ -2,5 +2,3 @@ from rest_framework import serializers
 from .models import User, Task, Friends, Ranking, Messages
 
 class serializer(serializers.ModelSerializer):
-    model = User
-    fields = '__all__'
