65e2d043f60e974ccc550a399e0588d2410c50c8
vicomarquez <vicomarquezmuniz@gmail.com>
2023-02-11 14:15:58 -0300

    add questions file

---
+++ src/preguntas.js
@@ -0,0 +1,92 @@
+const questions = [
+    {
+    question: "What does HTML stand for?",
+    options: [
+        { Answer: "HyperText Making Language", isCorrect: false },
+        { Answer: "HyperTextual Making Lists", isCorrect: true },
+        { Answer: "HyperText Markup Links", isCorrect: false },
+        { Answer: "HyperText Marks Language", isCorrect: false },
+    ],
+},
+{
+    question: "How can you start an HTML file?",
+    options: [
+        { Answer: "<html>", isCorrect: false },
+        { Answer: "<!DOCTYPE>", isCorrect: true },
+        { Answer: "<head>", isCorrect: false },
+        { Answer: "<body>", isCorrect: false },
+    ],
+},
+{
+    question: "The <head> and <body> tags are essential in this files. The <head> tag is a container for metadata (data about data) and the <body> contains all the contents of an HTML document, such as headings, paragraphs, images, hyperlinks, tables, lists, etc. Based on this, what are HTML tags?",
+    options: [
+        { Answer: "Instructions that tell a web browser how to format text.", isCorrect: true },
+        { Answer: "Structural elements used to identify the footer of a page", isCorrect: false },
+        { Answer: "Ways to give structure to the webpage", isCorrect: false },
+        { Answer: "Ways to style a webpage", isCorrect: false },
+    ],
+},
+{
+    question: "What are Attributes in HTML?",
+    options: [
+        { Answer: "Properties that style a webpage", isCorrect: false },
+        { Answer: "Ways to import links", isCorrect: false },
+        { Answer: "Properties you can add to an HTML tag", isCorrect: true },
+        { Answer: "Properties that change the main structure of the page", isCorrect: false },
+    ],
+},
+{
+    question: "Which tag declares a paragraph?",
+    options: [
+        { Answer: "<ul>", isCorrect: false },
+        { Answer: "<a>", isCorrect: false },
+        { Answer: "<p>", isCorrect: true },
+        { Answer: "<li>", isCorrect: false },
+    ],
+},
+{
+    question: "Which tag declares the size of a heading?",
+    options: [
+        { Answer: "<h>", isCorrect: true },
+        { Answer: "<hs>", isCorrect: false },
+        { Answer: "<s>", isCorrect: false },
+        { Answer: "<hr>", isCorrect: false },
+    ],
+},
+{
+    question: "Which tag lets you insert an image?",
+    options: [
+        { Answer: "<video>", isCorrect: false },
+        { Answer: "<audio>", isCorrect: false },
+        { Answer: "<img>", isCorrect: true },
+        { Answer: "<im>", isCorrect: false },
+    ],
+},
+{
+    question: "Which tag lets you define a hyperlink?",
+    options: [
+        { Answer: "<a>", isCorrect: true },
+        { Answer: "<link>", isCorrect: false },
+        { Answer: "<li>", isCorrect: false },
+        { Answer: "<l>", isCorrect: false },
+    ],
+},
+{
+    question: " How do you add a line break in the HTML file?",
+    options: [
+        { Answer: "<lb>", isCorrect: false },
+        { Answer: "<break>", isCorrect: false },
+        { Answer: "<b>", isCorrect: false },
+        { Answer: "<br>", isCorrect: true },
+    ],
+},
+{
+    question: "Which tag defines a thematic break in an HTML page?",
+    options: [
+        { Answer: "<tb>", isCorrect: false },
+        { Answer: "<br>", isCorrect: false },
+        { Answer: "<hr>", isCorrect: true },
+        { Answer: "<theme>", isCorrect: false },
+    ],
+},
+]
