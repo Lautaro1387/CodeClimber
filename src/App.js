ac44864179889378404f6a2ca83a25103ec667ce
MarceloRB1 <105667144+MarceloRB1@users.noreply.github.com>
2023-02-19 08:46:11 -0300

    update app.js

--- src/App.js
+++ src/App.js
@@ -7,10 +7,26 @@ import { Rank } from './components/ranking.js';
 import { data } from './js/dataRank.js';
 import Aboutus from "./components/about.js";
 import './styles/App.css'
+import About from './components/About';
+import Banner from './components/Banner';
+import Contact from './components/Contact';
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import ProgressBar from './components/ProgressBar';
+import Project from './components/Project';
+import UserProfile from './components/UserProfile';
 
 const App = () => {
+  const [username, setUsername] = useState('John Doe');
+
+  const handleUsernameChange = (newUsername) => {
+    setUsername(newUsername);
+  };
+
   return (
     <div className="App">
+      <Header />
+      <Navbar />
       <Routes>
         <Route path="/" element={<Log />} />
         <Route path="/home" element={<Menu name="Python" />} />
@@ -22,6 +38,12 @@ const App = () => {
         <Route path="/quiz/css/3" element={<Quiz />} />
         <Route path="/aboutus" element={<Aboutus />} />
         <Route path="/ranking" element={<Rank data={data} />} />
+        <Route path="/" element={<Banner username={username} />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/progress" element={<ProgressBar />} />
+        <Route path="/projects" element={<Project />} />
+        <Route path="/profile" element={<UserProfile onUsernameChange={handleUsernameChange} />} />
       </Routes>
     </div>
   );
