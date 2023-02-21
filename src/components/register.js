0deccb0262ff9fb78774b6a8e7a58818dcdda04e
m1dnas <felipesandimguliermone@gmail.com>
2023-02-21 01:05:07 -0300

    Conflictos de fusión solucionados

--- src/components/register.js
+++ src/components/register.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import swal from 'sweetalert';
@@ -151,60 +150,3 @@ export const Register = () => {
         </div>
     )
 }
-=======
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
-
-export const Register = (props) => {
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [pass, setPass] = useState('');
-
-    const navigate = useNavigate();
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        if (!email || !pass || !name) {
-            alert('Please fill in all fields');
-            return;
-        }
-        fetch('/api/users', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ name, email, password: pass })
-        })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        })
-        .then(data => {
-            console.log(data);
-            navigate('/home');
-        })
-        .catch(error => {
-            console.error('There was an error!', error);
-        });
-    }
-
-
-    return (
-        <div className="auth-form-container">
-            <h2>Register</h2>
-            <form className="register-form" onSubmit={handleSubmit}>
-                <label htmlFor="name">Username</label>
-                <input value={name} name="name" onChange={(e) => setName(e.target.value)} id="name" placeholder="Username" />
-                <label htmlFor="email">email</label>
-                <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="Enter your email" id="email" name="email" />
-                <label htmlFor="password">password</label>
-                <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
-                <button type="submit">Log In</button>
-            </form>
-            <button className="link-btn" onClick={() => props.onFormSwitch('login')}>Already have an account? Login here.</button>
-        </div>
-    )
-}
->>>>>>> 6902d243b420fb244ed35febe166a0ae7ef2c750
