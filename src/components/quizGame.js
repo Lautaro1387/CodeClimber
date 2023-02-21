35b4a985c314710e0c89cf398b6e37600c69f710
m1dnas <felipesandimguliermone@gmail.com>
2023-02-21 05:11:01 -0300

    URL config fixed

--- src/components/quizGame.js
+++ src/components/quizGame.js
@@ -46,7 +46,7 @@ export const Quiz = () => {
               Volver a jugar
             </button>
             <p></p>
-            <button onClick={() => (window.location.href = "home")}>
+            <button onClick={() => (window.location.href = "/home")}>
               Volver al inicio
             </button>
           </div>
