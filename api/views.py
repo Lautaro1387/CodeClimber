0f2c546fd76b331ad3221f3f7b9dfe2a9748ee35
m1dnas <felipesandimguliermone@gmail.com>
2023-02-25 23:45:50 -0300

    token added to signup

--- api/views.py
+++ api/views.py
@@ -1,7 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.pagination import PageNumberPagination
+from rest_framework.authtoken.models import Token
 
 from .serializers import UserSerializer, QuestionSerializer, SignUpSerializer
 
@@ -41,9 +41,12 @@ def users(request):
     elif request.method == 'POST':
         serializer = SignUpSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+            user = serializer.save()
+            token = Token.objects.create(user=user)
+            serializer_data = serializer.data
+            serializer_data['token'] = str(token.key)
+            return Response(serializer_data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET', 'PUT', 'DELETE'])
 def user_detail(request, user_id):
--- core/settings.py
+++ core/settings.py
@@ -44,6 +44,7 @@ PROJECT_APPS = [
 THIRD_PARTY_APPS = [
     'corsheaders',
     'rest_framework',
+    'rest_framework.authtoken',
     'ckeditor',
     'ckeditor_uploader',
 ]
@@ -137,7 +138,10 @@ AUTH_PASSWORD_VALIDATORS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10,
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+    ]
 }
 
 LANGUAGE_CODE = 'en-us'
